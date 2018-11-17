536f5d03d457eac0c75725c57f03a9f93f0a2055
Vladislav Kalashnikov-Travin <inayelle.arts@gmail.com>
2018-11-17 17:45:39 +0200

    change main message

--- App/Startup.cs
+++ App/Startup.cs
@@ -27,7 +27,7 @@ namespace App
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Privet obed!");
             });
         }
     }
