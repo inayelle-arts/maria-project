32eb5fbaf9e047f987e026f14c8aff691260e984
Vladislav Kalashnikov-Travin <inayelle.arts@gmail.com>
2018-12-20 23:03:21 +0200

    add typescript modularity

--- App/gulpfile.js
+++ App/gulpfile.js
@@ -2,7 +2,6 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const ts = require('gulp-typescript');
 const remove = require('gulp-clean');
 
 const staticRoot = './Static';
@@ -21,21 +20,8 @@ gulp.task('sass', function () {
                .pipe(gulp.dest(destStyles));
 });
 
-gulp.task('ts', function () {
-    return gulp.src(`${srcScripts}/**/*.ts`)
-               .pipe(ts({
-                            noImplicitAny: true
-                        }))
-               .on('error', console.log)
-               .pipe(gulp.dest(destScripts));
-});
-
 gulp.task('sass-watch', function () {
     return gulp.watch('./static/styles/scss/*.scss', 'sass');
-});
-
-gulp.task('ts-watch', function () {
-    return gulp.watch('./static/scripts/ts/*.ts', 'ts');
 });
 
 gulp.task('prepare-bootstrap', function () {
@@ -69,7 +55,7 @@ gulp.task('prepare-fa', function () {
 });
 
 gulp.task('clean', function () {
-    gulp.src(`${destScripts}/**/*`, {read: false})
+    gulp.src(`${destScripts}/**/*`, {read: false, allowEmpty: true})
         .pipe(remove());
 
     return gulp.src(`${destStyles}/**/*`, {read: false})
@@ -82,8 +68,7 @@ gulp.task(
         'prepare-fa',
         'prepare-bootstrap',
         'prepare-jquery',
-        'sass',
-        'ts'
+        'sass'
     )
 );
 
